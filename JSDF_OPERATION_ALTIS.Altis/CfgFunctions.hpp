daff891768b67e7ec475c3928d0b6b9b45ec39b2
JoramD0 <j.davids@hotmail.nl>
2018-01-16 02:11:52 +0100

    Moved LVR

--- JSDF_OPERATION_ALTIS.Altis/CfgFunctions.hpp
+++ JSDF_OPERATION_ALTIS.Altis/CfgFunctions.hpp
@@ -1,6 +1,13 @@
 class CfgFunctions {
     class ADDON {
         tag = QUOTE(ADDON);
+        class vehInitFncs
+        {
+            file = "functions\LVR";
+            class vehRespawn      	  {};
+            class hunterInit          {};
+            class littlebirdInit      {};
+        };
         class functions {
             file = "functions";
             class supplyBoxSpawn;
