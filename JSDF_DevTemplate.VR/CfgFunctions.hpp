6c05585152f50d2647dee2f50b28cb71004024a3
JoramD0 <j.davids@hotmail.nl>
2018-01-24 20:04:47 +0100

    Made seperate functions for the supplyBoxSpawn function (so I can call it elsewhere)

--- JSDF_DevTemplate.VR/CfgFunctions.hpp
+++ JSDF_DevTemplate.VR/CfgFunctions.hpp
@@ -10,7 +10,10 @@ class CfgFunctions {
         };
         class functions {
             file = "functions";
-            class supplyBoxSpawn;
+            class supplyBoxAmmo;
+            class supplyBoxMedical;
+            class supplBoxVehicle;
+            class supplyBoxController;
             class serviceVehicle;
             class arsenalBoxInit;
             class infoBriefing;
